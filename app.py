d43fe266008d4c0a7655899605cdc57e0bfedde3
felipemarianoferr <felipemf2@al.insper.edu.br>
2024-04-18 12:11:01 -0300

    post and read users

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask, request
 from flask_pymongo import PyMongo
 
 
-"""app = Flask("nome_da_minha_aplicacao")
-app.config["MONGO_URI"] = 
+app = Flask("nome_da_minha_aplicacao")
+app.config["MONGO_URI"] = "mongodb+srv://admin:admin@cluster0.ac4lm23.mongodb.net/biblioteca_db"
 mongo = PyMongo(app)
 
 
 @app.route('/usuarios', methods=['GET'])
 def get_all_users():
-    filtro = { "idade": { "$gt": 17 } }
+    filtro = {}
     projecao = {"_id": 0}
-    dados_usuarios = mongo.db.usuarios.find(filtro, projecao)
+    dados_usuarios = mongo.db.usuarios_aps_5.find(filtro, projecao)
 
     resp = {
         "usuarios": list(dados_usuarios)
@@ -19,6 +19,14 @@ def get_all_users():
 
     return resp, 200
 
+@app.route('/usuarios', methods=['POST'])
+def adicionar_usuario():
+
+    usuario = request.json
+
+    mongo.db.usuarios_aps_5.insert_one(usuario)
+
+    return {"mensagem": "Usuário adicionado com sucesso"}, 201
 
 if __name__ == '__main__':
-    app.run(debug=True)"""
+    app.run(debug=True)
