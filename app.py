aeb553a94230dffe3aabcb65c5d059bd76f17359
felipemarianoferr <felipemf2@al.insper.edu.br>
2024-04-19 10:22:17 -0300

    CRUD BIKES COMPLETO

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def obter_usuario(id):
         if dados_usuario:
             return {"usuarios": list(dados_usuario)}, 200
         else:
-            return {"erro": "Usuário não encontrado"}
+            return {"erro": "Usuário não encontrado"}, 404
 
 
 @app.route('/usuarios/<int:id>', methods=['PUT'])
@@ -97,6 +97,97 @@ def deletar_usuario(id):
             return {"mensagem": "Usuário deletado com sucesso"}, 200
         else:
             return {"erro": "Usuário não encontrado"}, 404
+
+#CRUD BIKES
+
+@app.route('/bikes', methods=['POST'])
+def adicionar_bicicleta():
+    bicicleta = request.json
+    id = bicicleta.get("id")
+
+    filtro = {"id": id}
+    projecao = {"_id": 0}
+    cursor = mongo.db.bicicletas_aps_5.find(filtro, projecao)
+    
+    # Convertendo o cursor em uma lista para facilitar a manipulação
+    dados_bicicletas = list(cursor)
+    
+    print(dados_bicicletas)
+    
+    if len(dados_bicicletas) > 0:
+        return {"erro": "Bicicleta já cadastrado"}, 400
+
+    mongo.db.bicicletas_aps_5.insert_one(bicicleta)
+
+    return {"mensagem": "Bicicleta adicionado com sucesso"}, 201
+
+
+@app.route('/bikes', methods=['GET'])
+def get_all_bikes():
+    filtro = {}
+    projecao = {"_id": 0}
+    dados_bicicletas = mongo.db.bicicletas_aps_5.find(filtro, projecao)
+
+    resp = {
+        "bicicletas": list(dados_bicicletas)
+    }
+
+    return resp, 200
+
+@app.route('/bikes/<int:id>', methods=['GET'])
+def obter_bicicleta(id):
+    try:
+        filtro = {"id": id}
+        projecao = {"_id": 0}
+        dados_bicicleta = list(mongo.db.bicicletas_aps_5.find(filtro, projecao))
+    except:
+        return {"erro": "Erro no sistema"}, 500
+    else:
+        if dados_bicicleta:
+            return {"bicicletas": list(dados_bicicleta)}, 200
+        else:
+            return {"erro": "Bicicleta não encontrada"}, 404
+
+@app.route('/bikes/<int:id>', methods=['PUT'])
+def editar_bicicleta(id):
+    try:
+        filtro = {"id": id}
+        projecao = {"_id": 0}
+        cursor = mongo.db.bicicletas_aps_5.find(filtro, projecao)
+        dados_bicicletas = list(cursor)
+    except:
+        return {"erro": "Erro no sistema"}, 500
+    else:
+        if len(dados_bicicletas) == 0:
+            return {"erro": "Bicicleta não encontrada"}, 404
+        else:
+            data = request.json
+            novos_dados = {
+                "$set": data
+            }
+
+            try:
+                mongo.db.bicicletas_aps_5.update_one(filtro, novos_dados)
+            except:
+                return {"erro": "Dados inválidos"}, 400
+
+            return {"mensagem": "Bicicleta atualizada com sucesso"}, 200
+
+
+@app.route('/bikes/<int:id>', methods=['DELETE'])
+def deletar_bicicleta(id):
+    try:
+        filtro = {"id": id}
+        projecao = {"_id": 0}
+        dados_bicicleta = list(mongo.db.bicicletas_aps_5.find(filtro, projecao))
+    except:
+        return {"erro": "Erro no sistema"}, 500
+    else:
+        if dados_bicicleta:
+            mongo.db.bicicletas_aps_5.delete_one(filtro)
+            return {"mensagem": "Bicicleta deletada com sucesso"}, 200
+        else:
+            return {"erro": "Bicicleta não encontrada"}, 404
 
 
 if __name__ == '__main__':
